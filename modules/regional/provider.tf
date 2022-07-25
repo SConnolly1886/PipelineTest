409bf7072a06340d6eb7d176080bfb2a320b0cdf
SConnolly1886 <sconnolly1886@gmail.com>
2022-07-25 18:41:24 -0400

    Update provider.tf

--- modules/regional/provider.tf
+++ modules/regional/provider.tf
@@ -1,26 +1,26 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.74"
-    }
-  }
-  required_version = "> 0.14"
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.74"
+#     }
+#   }
+#   required_version = "> 0.14"
+# }
 
-provider "aws" {
-  region = var.region
-  assume_role {
-    role_arn     = var.account
-    session_name = "INFRA_BUILD"
-  }
-}
+# provider "aws" {
+#   region = var.region
+#   assume_role {
+#     role_arn     = var.account
+#     session_name = "INFRA_BUILD"
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-2"
-  alias = "use2"
-  assume_role {
-    role_arn     = var.account
-    session_name = "INFRA_BUILD"
-  }
-}
+# provider "aws" {
+#   region = "us-east-2"
+#   alias = "use2"
+#   assume_role {
+#     role_arn     = var.account
+#     session_name = "INFRA_BUILD"
+#   }
+# }
